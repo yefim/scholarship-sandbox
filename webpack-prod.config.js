dded7a1d7090dc6795f0213cec3da7cbdf4580d3
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-09-28 03:24:08 -0700

    No need to push

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,12 +1,12 @@
 var webpack = require('webpack');
 var config = require('./webpack.config');
 
-config.plugins.push(
+config.plugins = [
   new webpack.DefinePlugin({
     'process.env': {
       'NODE_ENV': JSON.stringify('production')
     }
   })
-);
+];
 
 module.exports = config;
