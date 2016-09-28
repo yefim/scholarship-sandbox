84671c7dbf65a6654abe2d11e93f5a1c5bb880a5
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-09-28 00:35:35 -0700

    Reorder deploy so ssh is permitted first

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,15 @@ TARGET_BRANCH="gh-pages"
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in key.pub.enc -out key.pub -d
+chmod 600 key.pub
+eval `ssh-agent -s`
+ssh-add key.pub
+
 webpack -p --config webpack.config.js
 
 mkdir build/assets
@@ -18,14 +27,5 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 git add .
 git commit -m "Deploy to github pages"
-
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in key.pub.enc -out key.pub -d
-chmod 600 key.pub
-eval `ssh-agent -s`
-ssh-add key.pub
 
 git push --force --quiet $SSH_REPO $TARGET_BRANCH
