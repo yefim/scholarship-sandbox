fdd05db96b12d71ee0318171c2ea114f6c762af8
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-09-28 00:42:30 -0700

    Rename deploy_key

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,10 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in key.pub.enc -out key.pub -d
-chmod 600 key.pub
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
 eval `ssh-agent -s`
-ssh-add key.pub
+ssh-add deploy_key
 
 webpack -p --config webpack.config.js
 
